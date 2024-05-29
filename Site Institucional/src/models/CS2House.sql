43716808532d7314aa998cde2665642890af297e
FelipeGasp <felipe.gasparotto@sptech.school>
2024-05-29 20:38:31 -0300

    Lógica do quiz parcialmente feita

--- Site Institucional/src/models/CS2House.sql
+++ Site Institucional/src/models/CS2House.sql
@@ -7,6 +7,12 @@ email VARCHAR(45),
 senha VARCHAR(16),
 horas INT);
 
+CREATE TABLE habilidades (
+idHabilidade INT PRIMARY KEY AUTO_INCREMENT,
+role VARCHAR(10),
+fkUsuario INT,
+CONSTRAINT fkUsuario foreign key (fkUsuario) REFERENCES usuario(idUsuario);
+
 ALTER TABLE usuario MODIFY COLUMN senha VARCHAR(30);
 
 SELECT * FROM usuario;
